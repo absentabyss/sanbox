eabf5e3d6abd2157ac50704ba95e3649e408793f
absentabyss <agna.lumi@protonmail.com>
2020-08-02 17:44:34 -0300

    Made vim config global.

--- scripts/vim/gvim.sh
+++ scripts/vim/gvim.sh
@@ -1,2 +1,2 @@
 sudo pacman -S gvim &&
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo git clone https://github.com/VundleVim/Vundle.vim.git /etc/vim/bundle/Vundle.vim
